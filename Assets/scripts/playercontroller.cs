508aa9c33c303d129e051faa4acd9c1678aeb366
Bosssmann810 <sealered@gmail.com>
2025-09-24 09:25:42 -0300

    finished up to actually making the finished build

--- Assets/scripts/playercontroller.cs
+++ Assets/scripts/playercontroller.cs
@@ -1,16 +1,24 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
 
 public class playercontroller : MonoBehaviour
 {
     private Rigidbody rb;
     private float movementX;
-    private float movementY;
+    private float movementY; 
+    private int count;
     public float speed = 0;
+    public TextMeshProUGUI countText;
+    public GameObject winTextobject;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        count = 0;
+        winTextobject.SetActive(false);
+        SetCountText();
     }
 
   
@@ -20,9 +28,38 @@ public class playercontroller : MonoBehaviour
         movementX = movementvector.x;
         movementY = movementvector.y;
     }
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 12)
+        {
+            winTextobject.SetActive(true);
+            Destroy(GameObject.FindGameObjectWithTag("Enemy"));
+        }
+    }
     private void FixedUpdate()
     {
         Vector3 movement = new Vector3(movementX, 0.0f, movementY); 
         rb.AddForce(movement * speed);
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        { 
+            other.gameObject.SetActive(false);
+            count += 1;
+            SetCountText();
+        }
+            
+
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            Destroy(gameObject);
+            winTextobject.SetActive(true);
+            winTextobject.GetComponent<TextMeshProUGUI>().text = "You Lose"; 
+        }
     }
 }
